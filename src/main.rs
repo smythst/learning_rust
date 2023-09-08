88f8946ec64c2062796838098ae231ff4c164881
Bazooka Bob <smythst@gmail.com>
2023-09-08 15:47:17 +0100

    Variables & loops =================

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@
 
 fn main() {
 
-    //say hello
-    println!("Hello fishes!");
+    let arry = [1,2,3,4,5,6,7,8,9,10];
+    for (idx, val) in arry.iter().enumerate(){
+        println!("{}: {}", idx, val);
+    }
 
 }
-
